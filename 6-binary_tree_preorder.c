db8361af5fc461d298260f5710f46b9d9e5aa214
rrarima <5708@holbertonstudents.com>
2023-01-08 19:29:54 -0800

    task 6 attempt

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,8 +1,21 @@
 #include "binary_trees.h"
 
 /**
+  * binary_tree_preorder - function that goes through a binary tree using
+  *				pre-order traversa
   *
-  *
-  *
+  * @tree: pointer to root node of tree
+  * @func: pointer to func of each node
   */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+
+}
