bdf39d40c79c8f075a0b59cbe46b293bb374d2a0
rrarima <5708@holbertonstudents.com>
2023-01-08 20:51:26 -0800

    task 10 attempt

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,8 +1,25 @@
 #include "binary_trees.h"
 
 /**
+  * binary_tree_depth - function that measures the depth of a node
+  *			in a binary tree
   *
-  *
-  *
+  * @tree: pointer to the node to measure the depth
+  * Return: Depth of tree or 0 if tree is NULL
   */
+
 size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t i = 0;
+
+	if (tree == NULL)
+	{
+		return (i);
+	}
+	while (tree->parent != NULL)
+	{
+		i = i + 1;
+		tree = tree->parent;
+	}
+	return (i);
+}
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,9 +4,9 @@
   * binary_tree_is_leaf - check is if node is a leaf
   *
   * @node: pointer to node
-  *
   * Return: 1 if node is leaf, otherwise 0
   */
+
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node != NULL && node->left == NULL && node->right == NULL)
